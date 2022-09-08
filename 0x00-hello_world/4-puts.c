43d6c5ad1b2f4c67330f50bc5b80649a830169d4
Adefemi Oreyomi <olufy007@gmail.com>
2022-09-08 15:45:10 -0700

    Task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
  *
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
