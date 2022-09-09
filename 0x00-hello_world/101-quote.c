5ed55694a663b7b3ccc5e99487dabeda9706e216
Adefemi Oreyomi <olufy007@gmail.com>
2022-09-09 11:03:52 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - print "and that piece of art is useful" - Dora korpar, 2015-10-19
+ * main - print "and that piece of art is useful"
  * Return: 1 if the art is not
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n," 59);
 }
