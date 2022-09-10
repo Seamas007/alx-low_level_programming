480183061d55c75c298d410c1abedbb5e814c2b5
Adefemi Oreyomi <olufy007@gmail.com>
2022-09-10 14:33:30 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,15 +3,11 @@
 #include <time.h>
 /**
  * main - Entry point
-
- *
-
  * Return: Always 0 (Success)
-
  */
-
 int main(void)
-{int n;
+{
+int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
