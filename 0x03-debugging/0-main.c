865896cf23561aa6ee3d6bd491d2e31de00dc8d3
Adefemi Oreyomi <olufy007@gmail.com>
2022-09-15 10:47:41 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - print positive or negative integer
+ * Return:0
  */
+
 int main(void)
 {
 int i;
