826dc72eb57b9933ae34beeb399f332978b67c9c
“Adefemi <olufy007@gmail.com>
2022-09-19 06:47:22 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,14 @@
 #include "main.h"
-#include "stdio.h"
+
 /**
 *main - check the code
 *Return: Always 0.
 */
 int main(void)
 {
-int a;
-int b;
+int aux;
 
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
-return (0);
+aux = *a;
+*a = *b;
+*b = aux;
 }
