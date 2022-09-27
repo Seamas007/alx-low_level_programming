998fd9ea62e88dbabc5f97972c33fe8d5e8f2760
“Adefemi <olufy007@gmail.com>
2022-09-27 03:17:47 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * i_memset - fills memory with a constant byte.
+ * *_memset - fills memory with a constant byte.
  * @s: pointer to put the constant
  * @b: constant
  * @n: max bytes to use
