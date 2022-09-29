785d101e316bcb20f9f84d50f19b510878f22f14
“Adefemi <olufy007@gmail.com>
2022-09-29 06:25:06 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- * used in the 0x07-recursion directory.
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
@@ -16,5 +16,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
 #endif
