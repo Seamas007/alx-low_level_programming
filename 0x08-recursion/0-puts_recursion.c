163bc4c860ec0fed1e047090dcf3b589a9375f9f
“Adefemi <olufy007@gmail.com>
2022-09-29 07:23:17 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{	
-
-_putchar(*s);
-_puts_recursion(s + 1);
+if (*s == *\0)
+{
+_putchar("\n");
+return;
 }
-else
-_putchar('\n');
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
