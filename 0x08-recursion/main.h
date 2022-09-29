57e1ba3e10aa839488cf6f0823f28385ff6a4707
“Adefemi <olufy007@gmail.com>
2022-09-29 06:47:48 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,9 +4,8 @@
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and error is set appropriately.
  */
-int _putchar(char c)
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -16,4 +15,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 #endif
