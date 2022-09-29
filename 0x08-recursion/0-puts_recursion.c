4251cc99947ce5290f7296dbf15567ab5774bfd8
“Adefemi <olufy007@gmail.com>
2022-09-29 08:17:06 -0700

    Task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\n:')
-{
+if (*s == '\0')
 _putchar('\n');
-return;
-}
+else
+{
 _putchar(*s);
-s++;
-_puts_recursion(s);
+_puts_recursion(s + 1);
+}
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
+if (*s)
 {
-return;
-}
-s++;
-_print_rev_recursion(s);
-s--;
+_print_rev_recursion(s + 1);
 _putchar(*s);
+}
 }
