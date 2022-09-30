09d0382c2230d3575676e0e5ccdb121e956d9a94
“Adefemi <olufy007@gmail.com>
2022-09-29 20:18:29 -0700

    Task 0x08

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,8 +12,8 @@ return (1);
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-return (0);
-if (*s2 == '*' && *(s1 + 1) && *s2)
 return (1);
+if (*s2 == '*' && *(s1 + 1) && *s2)
+return (0);
 return (0);
 }
