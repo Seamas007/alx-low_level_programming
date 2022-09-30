b8b1df7b24daa6b594b53977c728cec4e12818e9
“Adefemi <olufy007@gmail.com>
2022-09-29 20:11:29 -0700

    Task 0x08

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,18 +5,15 @@
  * @s2: Second input string
  * Return: 1 if strings identical and 0 in otherwise
  */
-int wildcmip(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0' && *s2 == '\0')
+if (!*s1 && !*s2)
 return (1);
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
-if (*s2 == '*')
-{
-if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 return (0);
-if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+if (*s2 == '*' && *(s1 + 1) && *s2)
 return (1);
-}
 return (0);
 }
