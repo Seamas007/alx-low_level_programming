bf10c1ea48749d31d179b24a89f94de4a706c01e
“Adefemi <olufy007@gmail.com>
2022-10-01 05:15:36 -0700

    Task 0x0A

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
- * main - prints all the arguments a program receives.
- *
- * @argc: numiber of arguments
- *
- * @argv: array of arguments.
- * return: alawys (0) success.
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-int n;
-for (n = 0; n < argc; n++)
-printf("%s\n", argv[n]);
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
 return (0);
 }
