d668c6ec025c4f93bba0800d091f82be90233b8b
“Adefemi <olufy007@gmail.com>
2022-09-30 21:40:16 -0700

    Task 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+prinitf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
