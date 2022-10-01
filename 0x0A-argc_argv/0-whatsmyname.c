52349ce7aec6c2f9416251f4ab20f5b037f2a016
“Adefemi <olufy007@gmail.com>
2022-09-30 21:29:20 -0700

    Task 0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - Prints the name of the program
@@ -8,6 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[argc * 0]);
+printf("%s\n", argv[0]);
+(void)argc;
 return (0);
 }
