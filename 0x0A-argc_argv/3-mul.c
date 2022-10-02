618725dae650cbdb9fce70a2259f3b1eeaabce48
“Adefemi <olufy007@gmail.com>
2022-10-02 05:03:20 -0700

    Task 0x0A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,7 @@ if (argc < 3)
 printf("Error\n");
 return (1);
 }
-for (index = 1; index < argc; ibdex++)
+for (index = 1; index < argc; index++)
 {
 multiplication = multiplication *atoi(argv[index]);
 }
