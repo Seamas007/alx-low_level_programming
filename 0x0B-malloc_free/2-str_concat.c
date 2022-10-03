150788c9794a1ed13b2bc634f20bf0fcb56442b6
“Adefemi <olufy007@gmail.com>
2022-10-03 12:26:50 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,7 +26,7 @@ len2 = 0;
 while (s2[len2] != '\0')
 len2++;
 size = len1 + len2;
-nstr = malloc((sizeof(char) * size) + 1);
+nstr = malloc((sizeof(char) * size) +1);
 /*check if malloc was successful*/
 if (nstr == NULL)
 return (NULL);
