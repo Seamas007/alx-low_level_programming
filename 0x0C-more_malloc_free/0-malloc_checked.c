13f30f29986a95f77d02de3810af4ead09f6582a
“Adefemi <olufy007@gmail.com>
2022-10-07 02:47:36 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of memory to be allocated.
+ *
+ * Return: pointer to allocted memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
