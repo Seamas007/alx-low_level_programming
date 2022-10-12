f09513f4965510ade1446f83bef1dbfdfb06b681
“Adefemi <olufy007@gmail.com>
2022-10-12 07:32:45 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - Prints a name.
  * @name: The name to be printed.
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stdlib.h>
 void _putchar(char *c);
 void print_name(char *name, void (*f)(cahr *));
 void array_iterator(int *array, size_t size, void (*action)(int));
