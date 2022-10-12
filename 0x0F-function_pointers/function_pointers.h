35a1941f7fff703b883053611e0f20f4aaeeab91
“Adefemi <olufy007@gmail.com>
2022-10-12 06:53:17 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdlib.h>
 void _putchar(char *c);
 void print_name(char *name, void (*f)(cahr *));
 void array_iterator(int *array, size_t size, void (*action)(int));
