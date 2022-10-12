7f91597d6355923a0d8cb83e3ddbf8574c214e7e
“Adefemi <olufy007@gmail.com>
2022-10-12 06:38:15 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 /**
  *print_name - prints a name.
  *@name: pointer to name.
  *@f: function pointer.
  *
- *Return: void. 
+ *Return: void
  */
 void print_name(char *name, void (*f)(cahr *))
 {
