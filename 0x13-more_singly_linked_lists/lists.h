d547fb012830c1a0c08dc29d3e52ef46d8a843d4
“Adefemi <olufy007@gmail.com>
2022-10-24 18:07:42 -0700

    Task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+/*
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
 
 /**
@@ -18,19 +22,6 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
-typedef struct listp_s
-{
-void *p;
-struct listp_s *next;
-} listp_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -43,8 +34,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-isize_t print_listint_safe(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
