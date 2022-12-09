d9b6ba27ad263e0d6e0c12685a5ef85105add0b6
“Adefemi <myseamas@gmail.com>
2022-12-09 00:04:33 -0800

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,38 +1,24 @@
+/*
+ * File: 1-dlistint_len.c
+ */
+
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning
- * of a dlistint_t list
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
+ * Return: The number of elements in the dlistint_t list.
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t *new;
-	dlistint_t *h;
+	size_t nodes = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	h = *head;
-
-	if (h != NULL)
+	while (h)
 	{
-		while (h->prev != NULL)
-			h = h->prev;
+		nodes++;
+		h = h->next;
 	}
 
-	new->next = h;
-
-	if (h != NULL)
-		h->prev = new;
-
-	*head = new;
-
-	return (new);
+	return (nodes);
 }
